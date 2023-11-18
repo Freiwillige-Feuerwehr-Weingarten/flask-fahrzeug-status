b836e31473af4db03e42f9aa999edaeb093e4db8
Arne Usadel <arne.usadel@googlemail.com>
2023-11-18 15:47:58 +0100

    corrected uvicorn call

--- app/run.py
+++ app/run.py
@@ -124,4 +124,4 @@ async def status(request: Request, vehicle: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0, port=5000')
+    uvicorn.run(app, host="127.0.0.1", port=5000)
