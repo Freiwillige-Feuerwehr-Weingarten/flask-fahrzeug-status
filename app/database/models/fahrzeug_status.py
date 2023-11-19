ffd1cfa9b1c3cae65fb48f681ca8214055003735
Arne Usadel <arne.usadel@googlemail.com>
2023-11-19 22:30:45 +0100

    added basic layout

--- app/database/models/fahrzeug_status.py
+++ app/database/models/fahrzeug_status.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Integer, String, Column, TIMESTAMP
+from sqlalchemy import Integer, String, Column, TIMESTAMP, ForeignKey
 from app.database.db_setup import Base
+from app.database.models.fahrzeuge import Fahrzeuge
 
 class Fahrzeug_Status(Base):
     __tablename__ = "fahrzeug_status"
 
-    issi = Column(Integer, nullable=False)
+    issi = Column(Integer, ForeignKey("fahrzeuge.issi"), nullable=False)
     status = Column(String, nullable=False)
     timestamp = Column(TIMESTAMP(timezone=False))
     id = Column(Integer, index=True, primary_key=True, nullable=False)
