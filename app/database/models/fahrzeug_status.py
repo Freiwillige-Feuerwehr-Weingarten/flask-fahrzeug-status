fa0cfc05a7d5230f0ccb45e7399bf20d3d770c40
Arne Usadel <arne.usadel@googlemail.com>
2023-11-20 18:26:25 +0100

    do not link tables with fk

--- app/database/models/fahrzeug_status.py
+++ app/database/models/fahrzeug_status.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Integer, String, Column, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import relationship
 from app.database.db_setup import Base
 from app.database.models.fahrzeuge import Fahrzeuge
 
 class Fahrzeug_Status(Base):
     __tablename__ = "fahrzeug_status"
 
-    issi = Column(Integer, ForeignKey("fahrzeuge.issi"), nullable=False)
+    issi = Column(Integer, nullable=False)
     status = Column(String, nullable=False)
     timestamp = Column(TIMESTAMP(timezone=False))
     id = Column(Integer, index=True, primary_key=True, nullable=False)
-    # TODO: Need to indicate that issi is a foreign key
--- app/database/models/fahrzeuge.py
+++ app/database/models/fahrzeuge.py
@@ -1,8 +1,15 @@
 from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import relationship
+# from sqlmodel import Field, SQLModel
 from app.database.db_setup import Base
+
 
 class Fahrzeuge(Base):
     __tablename__ = "fahrzeuge"
 
     issi = Column(Integer, index=True, primary_key=True, nullable=False)
     funkrufname = Column(String(), index=True, nullable=False)
+
+#class Fahrzeuge(SQLModel, table=True):
+#    issi: int = Field(primary_key=True, nullable=False)
+#    funkrufname: str = Field(nullable=False)
