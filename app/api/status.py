71132c648db05cda5a973d5e3edf321e3f9da584
Arne Usadel <arne.usadel@googlemail.com>
2023-11-20 21:30:23 +0100

    Moved /status/is endpoint to where it belongs

--- app/api/status.py
+++ app/api/status.py
@@ -1,14 +1,29 @@
 import fastapi
 
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.db_setup import get_async_db
 from app.api.utils.status import get_status
 from app.pydantic_schemas.status import Status
+from app.db import get_async_pool
 
 status_router = fastapi.APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 @status_router.get("/status/", response_model=list[Status])
 async def aget_status(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Status]:
     status = await get_status(db)
     return status
+
+@status_router.get("/status/{vehicle}", response_class=HTMLResponse)
+async def aafahrzeug_status(request: Request, vehicle: str):
+    async with get_async_pool().connection() as conn:
+        async with conn.cursor() as cursor:
+            await cursor.execute("SELECT * FROM fahrzeug_status, fahrzeuge WHERE fahrzeug_status.issi = fahrzeuge.issi AND fahrzeuge.funkrufname = '%s' ORDER BY timestamp DESC" %vehicle)
+            records = await cursor.fetchall()
+            return templates.TemplateResponse("fahrzeug.html", {"request": request,
+                                                                "vehicle": vehicle,
+                                                                "records": records}) 
