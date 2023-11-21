5c2d8448d3727798c790f4fdc7db4f923b1a7e79
Arne Usadel <arne.usadel@googlemail.com>
2023-11-21 19:15:53 +0100

    added post route for status

--- app/api/status.py
+++ app/api/status.py
@@ -4,11 +4,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime
 
 from app.database.db_setup import get_async_db
 from app.api.utils.status import get_status
 from app.pydantic_schemas.status import Status
 from app.db import get_async_pool
+from app.database.models import status
 
 status_router = fastapi.APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -17,6 +19,19 @@ templates = Jinja2Templates(directory="templates")
 async def aget_status(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Status]:
     status = await get_status(db)
     return status
+
+
+@status_router.post("/api/status/", response_model=Status)
+async def handle_post_status(post: Status, db: AsyncSession = fastapi.Depends(get_async_db)) -> Status:
+
+    new_status = status.Status(issi=post.issi,
+           status=post.status,
+           timestamp=datetime.now(),
+           id=12345)
+    db.add(new_status)
+    await db.commit()
+    await db.refresh(new_status)
+    return new_status
 
 
 @status_router.get("/api/status/{issi}", response_model=list[Status])
