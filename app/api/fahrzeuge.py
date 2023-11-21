9d22858a934d47a45fb78e2664a8e78e4549dac3
Arne Usadel <arne.usadel@googlemail.com>
2023-11-21 22:25:03 +0100

    prefix methods with handle_ so they won't name-clash

--- app/api/fahrzeuge.py
+++ app/api/fahrzeuge.py
@@ -9,12 +9,12 @@ from app.pydantic_schemas.fahrzeuge import Fahrzeuge
 fahrzeuge_router = fastapi.APIRouter()
 
 @fahrzeuge_router.get("/vehicles/", response_model=list[Fahrzeuge])
-async def aget_vehicles(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Fahrzeuge]:
+async def handle_get_vehicles(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Fahrzeuge]:
     vehicles = await get_vehicles(db)
     return vehicles
 
 @fahrzeuge_router.post("/vehicles/", response_model=Fahrzeuge)
-async def apost_vehicles(post: Fahrzeuge, db: AsyncSession = fastapi.Depends(get_async_db)) -> Fahrzeuge:
+async def handle_post_vehicles(post: Fahrzeuge, db: AsyncSession = fastapi.Depends(get_async_db)) -> Fahrzeuge:
     new_vehicle = Fahrzeuge(issi=post.issi,
                             funkrufname=post.funkrufname)
     db.add(new_vehicle)
