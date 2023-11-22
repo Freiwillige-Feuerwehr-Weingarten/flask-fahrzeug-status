e5452755b530bcf7ffd8784ba9d820d0f34415d4
Arne Usadel <arne.usadel@googlemail.com>
2023-11-22 13:00:28 +0100

    changed responsee codes

--- app/api/fahrzeuge.py
+++ app/api/fahrzeuge.py
@@ -1,6 +1,7 @@
 import fastapi
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import exc
 
 from app.database.db_setup import get_async_db
 from app.database.models import fahrzeuge
@@ -14,11 +15,14 @@ async def handle_get_vehicles(db: AsyncSession = fastapi.Depends(get_async_db)) 
     vehicles = await get_vehicles(db)
     return vehicles
 
-@fahrzeuge_router.post("/api/vehicles/", response_model=Fahrzeuge)
+@fahrzeuge_router.post("/api/vehicles/", response_model=Fahrzeuge, status_code=fastapi.status.HTTP_201_CREATED)
 async def handle_post_vehicles(post: Fahrzeuge, db: AsyncSession = fastapi.Depends(get_async_db)) -> Fahrzeuge:
     new_vehicle = fahrzeuge.Fahrzeuge(issi=post.issi,
                             funkrufname=post.funkrufname)
-    db.add(new_vehicle)
-    await db.commit()
-    await db.refresh(new_vehicle)
+    try:
+        db.add(new_vehicle)
+        await db.commit()
+        await db.refresh(new_vehicle)
+    except exc.IntegrityError:
+        raise fastapi.exceptions.HTTPException(status_code=400, detail="Dupclicate Entry")
     return new_vehicle
