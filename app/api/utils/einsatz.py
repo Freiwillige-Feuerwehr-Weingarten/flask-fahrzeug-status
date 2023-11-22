67cca07e2c37ef64d6c4d9028168ec43e6d0a448
Arne Usadel <arne.usadel@googlemail.com>
2023-11-22 21:38:52 +0100

    helper method to lookup deployment.id with the help of an external_id and source

--- app/api/utils/einsatz.py
+++ app/api/utils/einsatz.py
@@ -6,3 +6,9 @@ async def get_deployments(db: AsyncSession):
     query = select(einsatz_model.Einsatz)
     result = await db.execute(query)
     return result.scalars().all()
+
+async def get_deployment_by_external_data(external_source: str, external_deployment_id: int, db: AsyncSession) -> einsatz_model.Einsatz:
+    query = select(einsatz_model.Einsatz).where(einsatz_model.Einsatz.external_deployment_id == external_deployment_id,
+                                                einsatz_model.Einsatz.external_source == external_source)
+    result = await db.execute(query)
+    return result.scalar_one()
