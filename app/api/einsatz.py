ecabe0e7016b0e42475817c5e2c3687cdaae442f
Arne Usadel <arne.usadel@googlemail.com>
2023-11-22 13:08:56 +0100

    changed import of einsatz schema, so it can be more explicit

--- app/api/einsatz.py
+++ app/api/einsatz.py
@@ -4,16 +4,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.db_setup import get_async_db
 from app.api.utils.einsatz import get_deployments
-from app.pydantic_schemas.einsatz import Einsatz
+from app.pydantic_schemas import einsatz_schema
 
 einsatz_router = fastapi.APIRouter()
 
-@einsatz_router.get("/api/deployments/", response_model=list[Einsatz])
-async def aget_deployments(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Einsatz]:
+@einsatz_router.get("/api/deployments/", response_model=list[einsatz_schema.Einsatz])
+async def aget_deployments(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[einsatz_schema.Einsatz]:
     deployments = await get_deployments(db)
     return deployments
 
 @einsatz_router.post("/api/deployments/")
-async def handle_post_deployments(post: Einsatz, db: AsyncSession = fastapi.Depends(get_async_db)) -> Einsatz:
+async def handle_post_deployments(post: einsatz_schema.Einsatz, db: AsyncSession = fastapi.Depends(get_async_db)) -> einsatz_schema.Einsatz:
     print(post)
     return post
