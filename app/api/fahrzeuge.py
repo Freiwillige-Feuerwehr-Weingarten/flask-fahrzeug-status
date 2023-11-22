9d2c92062c302c9eb429094bd1a8157e16909df7
Arne Usadel <arne.usadel@googlemail.com>
2023-11-22 13:06:47 +0100

    changed import of fahrzeuge schema, so it can be more explicit

--- app/api/fahrzeuge.py
+++ app/api/fahrzeuge.py
@@ -6,17 +6,17 @@ from sqlalchemy import exc
 from app.database.db_setup import get_async_db
 from app.database.models import fahrzeuge
 from app.api.utils.fahrzeuge import get_vehicles
-from app.pydantic_schemas.fahrzeuge import Fahrzeuge
+from app.pydantic_schemas import fahrzeuge_schema
 
 fahrzeuge_router = fastapi.APIRouter()
 
-@fahrzeuge_router.get("/api/vehicles/", response_model=list[Fahrzeuge])
-async def handle_get_vehicles(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[Fahrzeuge]:
+@fahrzeuge_router.get("/api/vehicles/", response_model=list[fahrzeuge_schema.Fahrzeuge])
+async def handle_get_vehicles(db: AsyncSession = fastapi.Depends(get_async_db)) -> list[fahrzeuge_schema.Fahrzeuge]:
     vehicles = await get_vehicles(db)
     return vehicles
 
-@fahrzeuge_router.post("/api/vehicles/", response_model=Fahrzeuge, status_code=fastapi.status.HTTP_201_CREATED)
-async def handle_post_vehicles(post: Fahrzeuge, db: AsyncSession = fastapi.Depends(get_async_db)) -> Fahrzeuge:
+@fahrzeuge_router.post("/api/vehicles/", response_model=fahrzeuge_schema.Fahrzeuge, status_code=fastapi.status.HTTP_201_CREATED)
+async def handle_post_vehicles(post: fahrzeuge_schema.Fahrzeuge, db: AsyncSession = fastapi.Depends(get_async_db)) -> fahrzeuge_schema.Fahrzeuge:
     new_vehicle = fahrzeuge.Fahrzeuge(issi=post.issi,
                             funkrufname=post.funkrufname)
     try:
